d084f2ed1092cf1bbb7195a8ac3cab534fc3300f
Amir Pourmand <pourmand1376@gmail.com>
2022-07-16 15:17:12 +0430

    change save dir

--- asriran/run_asriran.sh
+++ asriran/run_asriran.sh
@@ -1,2 +1,4 @@
-rm asriran/asriran.csv
-scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 asriran.py -o asriran/asriran.csv
+rm asriran/kaggle/asriran.csv
+scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 asriran.py -o asriran/kaggle/asriran.csv \
+    -a from_page=1 -a to_page=8313 \
+    -a from_date='1384/01/01' -a to_date='1401/04/24'
--- tasnim/run_tasnim.sh
+++ tasnim/run_tasnim.sh
@@ -1,2 +1,2 @@
-rm tasnim/tasnim.csv
-scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 tasnim.py -o tasnim/tasnim.csv
+rm tasnim/kaggle/tasnim.csv
+scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 tasnim.py -o tasnim/kaggle/tasnim.csv 
