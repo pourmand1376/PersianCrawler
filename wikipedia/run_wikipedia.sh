586af605eb80a56ac9588a3b40301e79944e6050
Amir Pourmand <pourmand1376@gmail.com>
2022-07-17 12:07:19 +0430

    add starting point

--- wikipedia/run_wikipedia.sh
+++ wikipedia/run_wikipedia.sh
@@ -1,5 +1,5 @@
 while true; do
-    read -p "Do you want to build index.txt file? " yn
+    read -p "Do you want to build index.txt file? (This is required for the first time. Crawler uses index.txt as starting point) " yn
     case $yn in
         [Yy]* ) rm index.txt
                 scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 wikipedia.py -o wikipedia/kaggle/wikipedia.csv -a gather_index_pages=True; exit;;
