930dfa5bd23f2c9ea96f4dfd183699b1792049f7
Amir Pourmand <pourmand1376@gmail.com>
2022-07-17 11:39:30 +0430

    add index

--- wikipedia/run_wikipedia.sh
+++ wikipedia/run_wikipedia.sh
@@ -2,7 +2,7 @@ while true; do
     read -p "Do you wish to remove this run and run crawler again? " yn
     case $yn in
         [Yy]* ) rm wikipedia/kaggle/wikipedia.csv
-                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 wikipedia.py -o wikipedia/kaggle/wikipedia.csv; break;;
+                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 wikipedia.py -o wikipedia/kaggle/wikipedia.csv -a gather_index_pages=True; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
