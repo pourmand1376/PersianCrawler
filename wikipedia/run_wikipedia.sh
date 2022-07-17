7f3a9939167e1016ff9bc29457950a72937d5aff
Amir Pourmand <pourmand1376@gmail.com>
2022-07-17 11:47:55 +0430

    add wikipedia

--- wikipedia/run_wikipedia.sh
+++ wikipedia/run_wikipedia.sh
@@ -1,4 +1,14 @@
 while true; do
+    read -p "Do you want to build index.txt file? " yn
+    case $yn in
+        [Yy]* ) rm wikipedia/kaggle/wikipedia.csv
+                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 wikipedia.py -o wikipedia/kaggle/wikipedia.csv -a gather_index_pages=True; exit;;
+        [Nn]* ) echo "You chose not to build index.txt file. Going forwrd to crawl the wikipedia"; break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
     read -p "Do you wish to remove this run and run crawler again? " yn
     case $yn in
         [Yy]* ) rm wikipedia/kaggle/wikipedia.csv
@@ -7,3 +17,4 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+
