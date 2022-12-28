787c93e151768ee2526ebae38f2d1652be56aff1
Amir Pourmand <pourmand1376@gmail.com>
2022-12-28 14:11:27 +0330

    add isan

---
+++ isna.py
@@ -0,0 +1,74 @@
+import scrapy
+import re
+from logger import logger
+
+
+class IsnaSpider(scrapy.Spider):
+    
+    name = "Isna"
+    start_urls = []
+
+    main_url = "https://www.isna.ir"
+    
+    def __init__(self ,from_year = 1378, to_year = 1401):
+        # Set the range of values for the mn, dy, and yr parameters
+        mn_range = range(1,2) # 32)
+        dy_range = range(1,2)# 13)
+        yr_range = range(int(from_year),int(to_year)+1)
+        pi_range= range(1,101)
+        # Create an empty list to store the start URLs
+        self.start_urls = []
+
+        # Loop through the possible values for each parameter
+        for mn in mn_range:
+            for dy in dy_range:
+                for yr in yr_range:
+                    for pi in pi_range:
+                        # Construct the URL using string formatting
+                        url = f"https://www.isna.ir/archive?pi={pi}&ms=0&dy={dy}&mn={mn}&yr={yr}"
+                        # Add the URL to the start_urls list
+                        self.start_urls.append(url)
+
+        # Print the start URLs
+        logger.info('urls are appended')
+
+
+    def parse(self, response ):
+        try:
+            for news in response.css("div.items a::attr(href)").getall():
+                if len(news.strip()) > 0:
+                    yield scrapy.Request(
+                        self.main_url+news,
+                        callback=self.parse_news,
+                    )
+                    logger.info('added '+ self.main_url+news)
+        except Exception:
+            logger.error("Parsing Error: ", exc_info=True)
+
+    def parse_news(self, response):
+        try: 
+            # I used pip install scrapy and scrapy shell to help me generate this content. 
+            # scrapy shell
+            # fetch(url)
+            # then use reponse.css
+            # also Copy CSS Selector was useful
+            item = {
+                'title': response.css('article#item h1::text').get(),
+                'shortlink': response.css('input#short-url::attr(value)').get(),
+                'time':  response.css('article#item li:nth-child(1) > span.text-meta::text').get(),
+                'service': response.css('article#item li:nth-child(2) > span.text-meta::text').get(),
+                'news_id': response.css('article#item li:nth-child(3) > span.text-meta::text').get(),
+                'reporter': response.css('article#item li:nth-child(1) > strong::text').get(), 
+                'managers': response.css('article#item li:nth-child(2) > strong::text').get(),
+                'body': ' '.join(response.css('article#item div.item-body *::text').getall()),
+            }
+            
+            # if I use strip on all of them I may get error. I have to check if it is not none. 
+            for key in item:
+                if item[key]:
+                    item[key] = re.sub(' +', ' ', item[key]).strip()
+            
+            yield item
+
+        except Exception:
+            logger.error("Error", exc_info=True)
--- isna/run_isna.sh
+++ isna/run_isna.sh
@@ -1,8 +1,8 @@
 while true; do
     read -p "Do you wish to remove this run and run crawler again? " yn
     case $yn in
-        [Yy]* ) rm tasnim/kaggle/tasnim.csv
-                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 tasnim.py -o tasnim/kaggle/tasnim.csv ; break;;
+        [Yy]* ) rm isna/kaggle/isna.csv
+                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 isna.py -o isna/kaggle/isna.csv  -a from_year=1401 -a to_year=1401 ; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
