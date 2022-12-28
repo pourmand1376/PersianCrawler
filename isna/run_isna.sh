d67aca1f16a6f3c305507598e7af9d157a8f0c1c
Amir Pourmand <pourmand1376@gmail.com>
2022-12-28 15:44:59 +0330

    add isna

--- isna/run_isna.sh
+++ isna/run_isna.sh
@@ -2,7 +2,7 @@ while true; do
     read -p "Do you wish to remove this run and run crawler again? " yn
     case $yn in
         [Yy]* ) rm isna/kaggle/isna.csv
-                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 isna.py -o isna/kaggle/isna.csv  -a from_year=1390 -a to_year=1401 ; break;;
+                scrapy runspider --set FEED_EXPORT_ENCODING=utf-8 isna.py -o isna/kaggle/isna.csv  -a from_year=1378 -a to_year=1401 ; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
